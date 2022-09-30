4ae5d6ee38f2c8e41675f3982f45acc3d42e6ae5
Jar270 <112799538+Jar270@users.noreply.github.com>
2022-09-30 10:31:25 -0700

    added a button
    No longer have to refresh to play. Just press the button.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-// p1 dice
-let randomNum1 = Math.ceil(Math.random() * 6);
-console.log(randomNum1);
-let randomImg1 = "images/dice" + randomNum1 + ".png";
-document.querySelector("img.img1").setAttribute("src", randomImg1);
 
-//p2 dice
-let randomNum2 = Math.ceil(Math.random() * 6);
-console.log(randomNum2);
-let randomImg2 = "images/dice" + randomNum2 + ".png";
-document.querySelector("img.img2").setAttribute("src", randomImg2);
 
-//winner
-if(randomNum1 > randomNum2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-}
-else if (randomNum1 == randomNum2) {
-    document.querySelector("h1").innerHTML = "Tie!";
-}
-else {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+//roll button
+document.querySelector("button").addEventListener("click", rollDice);
+
+function rollDice() {
+    // p1 dice
+    let randomNum1 = Math.ceil(Math.random() * 6);
+    console.log(randomNum1);
+    let randomImg1 = "images/dice" + randomNum1 + ".png";
+    document.querySelector("img.img1").setAttribute("src", randomImg1);
+
+    //p2 dice
+    let randomNum2 = Math.ceil(Math.random() * 6);
+    console.log(randomNum2);
+    let randomImg2 = "images/dice" + randomNum2 + ".png";
+    document.querySelector("img.img2").setAttribute("src", randomImg2);
+
+    //winner
+    if(randomNum1 > randomNum2) {
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    }
+    else if (randomNum1 == randomNum2) {
+        document.querySelector("h1").innerHTML = "Tie!";
+    }
+    else { 
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    }
 }
